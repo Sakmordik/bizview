b67236095458d9232caa4327715990bcaa26c262
Sakmordik <89955644+Sakmordik@users.noreply.github.com>
2021-09-23 20:44:11 +0100

    Bizview Deplot GHPages

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() { 
+        return <div className="nav p-3 mb-2 bg-dark text-white" >
+            <h1>
+                BizView Application Form
+            </h1>
+        </div>
+    }
+}
+ 
+export default Header;
